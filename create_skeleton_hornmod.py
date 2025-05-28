15345faf63feef63167e2bb8edc6b70ff02c0b80
Christian Angel <chrisangel256@gmail.com>
2025-05-27 20:58:21 -0400

    Add script to create skeleton hornmod with no horns and just code

---
+++ create_skeleton_hornmod.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import shutil
+import hashlib
+import zipfile
+import urllib.request
+
+
+if __name__ == '__main__':
+    output_filename = sys.argv[1]
+    assert not os.path.exists(output_filename)
+    stock_hornmod_path = os.path.join('resources', 'KL_HORNMOD-CE_V2.pk3')
+    if not os.path.exists(stock_hornmod_path):
+        print(f'{stock_hornmod_path} not found. Attempting to download...')
+        urllib.request.urlretrieve('https://mb.srb2.org/addons/hornmod-championship-edition.2418/download', stock_hornmod_path)
+        with open(stock_hornmod_path, 'rb') as stock_hornmod_file:
+            stock_hornmod_hash = hashlib.sha512(stock_hornmod_file.read())
+            assert stock_hornmod_hash.hexdigest() == 'fff66dd83bd2da9863904b18ca6d20498829796f95ed37d17449a7236d1cd4e42fe683a7624a64d92f58d69ef00e80d13a365ade6a38be80da80a7f122155e6c'
+        print(f'{stock_hornmod_path} downloaded and verified!')
+
+    with zipfile.ZipFile(stock_hornmod_path, 'r') as input_pk3_file:
+        with zipfile.ZipFile(output_filename, 'w') as output_pk3_file:
+            #output_pk3_file.mkdir('sfx')
+            #output_pk3_file.mkdir('sprites')
+            def copy_packed_file(input_path, output_path):
+                with input_pk3_file.open(input_path, 'r') as source_file:
+                    with output_pk3_file.open(output_path, 'w') as dest_file:
+                        shutil.copyfileobj(source_file, dest_file)
+            for filepath in input_pk3_file.namelist():
+                if filepath.lower().startswith('sfx') or filepath.lower().startswith('sprites'):
+                    copy_packed_file(filepath, filepath)
+            output_pk3_file.mkdir('lua')
+            for filename in ['hornmod-ce.lua', 'config.lua', 'pseudorng.lua']:
+                copy_packed_file('lua/' + filename, 'lua/' + filename)
