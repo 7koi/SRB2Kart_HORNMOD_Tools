e8453022399add4e59fb817f4eafa6aaafd8d220
Christian Angel <chrisangel256@gmail.com>
2025-05-27 21:01:33 -0400

    Update build_hornmod.py such that it creates booster hornmods instead of standalone hornmods

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 In every hornmod, there is a Lua script that has calls to HORNMOD_AddHorns() that serves as an index of the horns within the mod. This index contains their in-archive filenames and their titles. If you have a hornmod and know the in-archive path to the index Lua script, you can extract the horns from it with this command:
 `python extract_horns.py path/to/hornmod.pk3 archive/path/to/lua/horn/index`
 
-Once you have all your horns in their respective directories, you can build your own custom hornmod:
+Once you have all your horns in their respective directories, you can build your own custom booster hornmod:
 `python build_hornmod.py path/to/output.pk3`
+
+If you want to create a skeleton hornmod with 0 horns in it but all the scripts to support booster hornmods, use this command:
+`python create_skeleton_hornmod.py path/to/output.pk3`
--- build_hornmod.py
+++ build_hornmod.py
@@ -2,11 +2,9 @@
 
 import os
 import sys
-import glob
 import shutil
 import hashlib
-import subprocess
-import urllib.request
+import zipfile
 
 
 def horns_to_lua(horns, hell=False):
@@ -17,60 +15,35 @@ def horns_to_lua(horns, hell=False):
     return lua
 
 
-def copy_horns(sourcedir, destdir):
+def copy_horns_to_archive(sourcedir, destdir, archive):
     horns = []
     for ogg_file in os.listdir(sourcedir):
-        if ogg_file == '.gitignore':
+        if '.gitignore' in ogg_file:
             continue
         ogg_path = os.path.join(sourcedir, ogg_file)
         ogg_hash = hashlib.sha1(ogg_file.encode('utf-8')).hexdigest()[:6].upper()
         if ogg_file.lower().endswith('.ogg'):
             ogg_file = ogg_file[:-4]
         horns.append((ogg_file, ogg_hash))
-        shutil.copy(ogg_path, os.path.join(destdir, f'DS{ogg_hash}.ogg'))
+        with open(ogg_path, 'rb') as source_file:
+            with archive.open(os.path.join(destdir, f'DS{ogg_hash}.ogg'), 'w') as dest_file:
+                shutil.copyfileobj(source_file, dest_file)
     return horns
-
-
-def clean_dir(dirpath):
-    for horn_filename in os.listdir(dirpath):
-        os.remove(os.path.join(dirpath, horn_filename))
 
 
 if __name__ == '__main__':
     output_filename = sys.argv[1]
     assert not os.path.exists(output_filename)
-    stock_hornmod_path = os.path.join('resources', 'KL_HORNMOD-CE_V2.pk3')
-    if not os.path.exists(stock_hornmod_path):
-        print(f'{stock_hornmod_path} not found. Attempting to download...')
-        urllib.request.urlretrieve('https://mb.srb2.org/addons/hornmod-championship-edition.2418/download', stock_hornmod_path)
-        with open(stock_hornmod_path, 'rb') as stock_hornmod_file:
-            stock_hornmod_hash = hashlib.sha512(stock_hornmod_file.read())
-            assert stock_hornmod_hash.hexdigest() == 'fff66dd83bd2da9863904b18ca6d20498829796f95ed37d17449a7236d1cd4e42fe683a7624a64d92f58d69ef00e80d13a365ade6a38be80da80a7f122155e6c'
-        print(f'{stock_hornmod_path} downloaded and verified!')
 
-    working_dir = 'working_directory'
-    os.mkdir(working_dir)
-    shutil.unpack_archive(stock_hornmod_path, format='zip', extract_dir=working_dir)
-    nephorns_path = os.path.join(working_dir, 'nephorns')
-    clean_dir(nephorns_path)
-    clean_dir(os.path.join(working_dir, 'horns'))
-    clean_dir(os.path.join(working_dir, 'hellhorns'))
+    with zipfile.ZipFile(output_filename, 'w') as output_pk3_file:
+        output_pk3_file.mkdir('horns')
+        output_pk3_file.mkdir('hellhorns')
 
-    os.remove(os.path.join(working_dir, 'lua', 'basehorns.lua'))
-
-    shutil.copy(os.path.join('resources', 'init.lua'), working_dir)
-
-    horns = copy_horns('horns', nephorns_path)
-    hellhorns = copy_horns('hellhorns', nephorns_path)
-    lua_lines = horns_to_lua(horns)
-    hell_lua_lines = horns_to_lua(hellhorns, hell=True)
-    lua_text = lua_lines + hell_lua_lines
-    with open(os.path.join(working_dir, 'lua', 'basehorns.lua'), 'w') as lua_file:
-        lua_file.write(lua_text)
-
-    cwd = os.getcwd()
-    os.chdir(working_dir)
-    subprocess.run(['zip', '-r', os.path.join('..', output_filename)] + glob.glob('*'))
-    os.chdir(cwd)
-
-    shutil.rmtree(working_dir)
+        horns = copy_horns_to_archive('horns', 'horns', output_pk3_file)
+        hellhorns = copy_horns_to_archive('hellhorns', 'hellhorns', output_pk3_file)
+        lua_lines = horns_to_lua(horns)
+        hell_lua_lines = horns_to_lua(hellhorns, hell=True)
+        lua_text = lua_lines + hell_lua_lines
+        output_pk3_file.mkdir('lua')
+        with output_pk3_file.open(os.path.join('lua', 'basehorns.lua'), 'w') as lua_file:
+            lua_file.write(lua_text.encode('utf-8'))
